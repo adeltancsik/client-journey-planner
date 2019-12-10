36e5985734d9e77b9e8f01ccb74aadebc1e6623a
adeltancsik <tancsikadel@gmail.com>
2019-12-10 09:59:34 +0100

    Display journey list from db backend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
+import { loadJourneys } from "../actions/journeys";
+import { connect } from "react-redux";
+import JourneyList from "./JourneyList";
 
 class Home extends Component {
+  componentDidMount() {
+    this.props.loadJourneys();
+  }
   render() {
-    return <div className="home">hi</div>;
+    return (
+      <div className="home">
+        {/* {!this.props.user ? (
+      <div>
+        <p>
+          If you don't have an account yet, please{" "}
+          <Link to={`/signup`}>Sign Up</Link>!
+        </p>
+        <LoginFormContainer />
+      </div>
+    ) : (
+      <AddEventFormContainer />
+    )} */}
+        <JourneyList journeys={this.props.journeys} />
+      </div>
+    );
   }
 }
 
-export default Home;
+const mapStateToProps = state => ({
+  journeys: state.journeys
+  // user: state.user
+});
+
+export default connect(mapStateToProps, { loadJourneys })(Home);
---
+++ src/components/JourneyList.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card, CardDeck } from "react-bootstrap";
+import moment from "moment";
+// import "./styling/EventList.css";
+
+export default function JourneyList(props) {
+  return (
+    <main>
+      {!props.journeys ? (
+        "Loading..."
+      ) : (
+        <div
+          className="container"
+          style={{
+            maxWidth: "70rem"
+          }}
+        >
+          <CardDeck className="container">
+            {props.journeys.map(journey => {
+              const date = moment(journey.startDate, "YYYY-MM-DD").fromNow();
+              console.log("true?", date.includes("in"));
+              return (
+                <Card
+                  style={{
+                    minWidth: "18rem",
+                    maxWidth: "18rem",
+                    margin: "1rem"
+                  }}
+                  className="text-center"
+                  border="info"
+                  key={journey.id}
+                >
+                  <Link to={`/journeys/${journey.id}`} className="link">
+                    {/* <Card.Img
+                      variant="top"
+                      src={journey.url}
+                      alt={journey.name}
+                    /> */}
+                    <Card.Header>{journey.country}</Card.Header>
+                  </Link>
+                  <Card.Body style={{ maxHeight: "12rem", overflow: "auto" }}>
+                    <Link to={`/journeys/${journey.id}`} className="link">
+                      <Card.Title>{journey.name}</Card.Title>
+                    </Link>
+                    {/* <Card.Text>{journey.country}</Card.Text> */}
+                  </Card.Body>
+                  <Card.Footer>
+                    <small className="text-muted">
+                      {date.includes("in") ? "Starts " : "Ended "}
+                      {date}
+                    </small>
+                  </Card.Footer>
+                </Card>
+              );
+            })}
+          </CardDeck>
+        </div>
+      )}
+    </main>
+  );
+}
